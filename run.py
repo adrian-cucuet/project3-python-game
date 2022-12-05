0e93079fccb93f8b9fbaf67d5cdb32740b939919
adrian-cucuet <adrian.cucuet@hotmail.com>
2022-12-05 09:16:39 +0000

    Import pyfiglet and emoji. Add intro game

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pyfiglet
+import emoji
+
+""" 
+Printing the Game Name
+"""
+game_name = pyfiglet.figlet_format("THE MANSION")
+print(game_name)
+
+player_name = input('Enter your name:\n')
+print(f"Hello, {player_name} \U0001F603")
+print("This game is a quest where you choose how the game ends.")
+print("You will need to answer questions and take decissions,\nin order to move through the game.")
+print("It's 3:30 AM. The phone's ringing. Who could it be at this time of night?")
+print("You answer. A voice at the other end says that it's the police. Your uncle, a noted scientist, has been found dead.")
+print("Time to investigate the house for any clues to his death...")
+print(f"Good luck \U0001F929 {player_name}!\n")
+print("You are standing at entrance of the mansion.")
+print("The gothic architecture lends a creepy feel to the entrance hall. Candles light the room.")
+print("You can go FORWARD to the other side of the entrance hall.")
+print("There is also a door to your LEFT.")
+print("You can see a glass-paned door to the RIGHT. Through it, a garden of some sort is visible.")
